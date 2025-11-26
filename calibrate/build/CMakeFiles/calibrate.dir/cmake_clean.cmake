0fecff74eef20d0fd5b4ca44ab092d54e18fc2b6
ZqZSFTJ <lzj2021414213@outlook.com>
2025-11-26 21:17:56 +0800

    calibrate

---
+++ calibrate/build/CMakeFiles/calibrate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibrate.dir/home/zqz/ros2_ws/hik_drive/src/HikCamera.cpp.o"
+  "CMakeFiles/calibrate.dir/home/zqz/ros2_ws/hik_drive/src/HikCamera.cpp.o.d"
+  "CMakeFiles/calibrate.dir/src/Calibration.cpp.o"
+  "CMakeFiles/calibrate.dir/src/Calibration.cpp.o.d"
+  "calibrate"
+  "calibrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
