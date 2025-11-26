03d3b610ac7ea8afa7bf4fe563235917dfdf71f6
ZqZSFTJ <lzj2021414213@outlook.com>
2025-11-26 21:26:03 +0800

    ui

---
+++ ui_design/build/CMakeFiles/MapVisualizer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MapVisualizer.dir/src/MapVisualizer.cpp.o"
+  "CMakeFiles/MapVisualizer.dir/src/MapVisualizer.cpp.o.d"
+  "CMakeFiles/MapVisualizer.dir/src/main.cpp.o"
+  "CMakeFiles/MapVisualizer.dir/src/main.cpp.o.d"
+  "MapVisualizer"
+  "MapVisualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MapVisualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
